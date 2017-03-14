160961931e7fc93f2368a9957c6689ee639bc12d
mdobrychlop <mateusz.dobrychlop@gmail.com>
2017-03-14 12:48:44 +0100

    Added simple motility

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
-import pygame
-import sys
-import math
 from pygame.locals import *
+import math
+import pygame
+import random
+import sys
 
 
 class BactSprite(pygame.sprite.Sprite):
-    MAX_FORWARD_SPEED = 10
-    MAX_REVERSE_SPEED = 10
-    ACCELERATION = 2
-    TURN_SPEED = 5
 
-    def __init__(self, position):
+    def __init__(self, position, src_image):
         pygame.sprite.Sprite.__init__(self)
-        self.src_image = pygame.image.load('basic.png')
+        self.src_image = pygame.image.load(src_image)
         self.position = position
         self.direction = 0
         self.speed = 0
-        self.k_left = 0
-        self.k_right = 0
-        self.k_up = 0
-        self.k_down = 0
+
+    def motility(self, windowsize, ticks):
+        switch_freq = 10
+        max_speed = 10
+        min_speed = 0
+        turn_angle = 40
+        bounce_speed = 5
+        bounce_margin = 50
+
+        if ticks % switch_freq == 0:
+            rand_speed = random.randrange(min_speed, max_speed)
+            self.speed = (rand_speed)
+            rand_direction = random.randrange(-turn_angle, turn_angle)
+            self.direction += (rand_direction)
+
+            if self.position[0] >= windowsize[0] - bounce_margin:
+                self.direction = 90
+                self.speed = (bounce_speed)
+            if self.position[1] >= windowsize[1] - bounce_margin:
+                self.direction = 0
+                self.speed = (bounce_speed)
+            if self.position[0] <= bounce_margin:
+                self.direction = -90
+                self.speed = (bounce_speed)
+            if self.position[1] <= bounce_margin:
+                self.direction = 180
+                self.speed = (bounce_speed)
 
     def update(self, deltat):
-        self.speed += (self.k_up + self.k_down)
-        if self.speed > self.MAX_FORWARD_SPEED:
-            self.speed = self.MAX_FORWARD_SPEED
-        if self.speed < -self.MAX_REVERSE_SPEED:
-            self.speed = -self.MAX_REVERSE_SPEED
-        self.direction += (self.k_right + self.k_left)
         x, y = self.position
         rad = self.direction * math.pi / 180
         x += -self.speed*math.sin(rad)
@@ -37,33 +51,30 @@ class BactSprite(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = self.position
 
+winx = 800
+winy = 600
+windowsize = (winx, winy)
+window = pygame.display.set_mode(windowsize)
 
-window = pygame.display.set_mode((640, 480))
-rect = window.get_rect()
-bact = BactSprite(rect.center)
+draw_coord = (random.randrange(100, winx-100), random.randrange(100, winy-100))
+
+bact = BactSprite(draw_coord, 'black_test3.png')
 bact_group = pygame.sprite.RenderPlain(bact)
 clock = pygame.time.Clock()
 
 while 1:
     deltat = clock.tick(60)
+
+    bact.motility(windowsize, pygame.time.get_ticks())
+
     for event in pygame.event.get():
         if not hasattr(event, 'key'):
             continue
-
-        down = event.type == KEYDOWN
-        if event.key == K_RIGHT:
-            bact.k_right = down * -5
-        if event.key == K_LEFT:
-            bact.k_left = down * 5
-        if event.key == K_UP:
-            bact.k_up = down * 2
-        if event.key == K_DOWN:
-            bact.k_down = down * -2
         if event.type == pygame.QUIT or event.key == K_ESCAPE:
             pygame.quit()
             sys.exit()
 
-    window.fill((0, 0, 0))
+    window.fill((150, 150, 150))
     bact_group.update(deltat)
     bact_group.draw(window)
     pygame.display.flip()
